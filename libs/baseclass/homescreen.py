77047ab6d51f840399c5a5499096951b26b4e037
_jonsonkurt <jonsonkurt@gmail.com>
2023-01-12 00:21:47 +0800

    Updated homescreen frontend and backend

--- libs/baseclass/homescreen.py
+++ libs/baseclass/homescreen.py
@@ -4,12 +4,31 @@ from kivymd.toast.kivytoast import toast
 from kivy.uix.screenmanager import Screen
 from kivymd.uix.card import MDCard
 from kivymd.uix.dialog import MDDialog
-from kivy.properties import StringProperty
-
+from datetime import datetime
+from kivy.properties import StringProperty, NumericProperty
 
 Builder.load_file('./libs/kv/homescreen.kv')
 
 class HomeScreen(Screen):
+    
+    time_date = StringProperty()
+    plant_status = StringProperty()
+    security_status = StringProperty()
+    plant_status_color = NumericProperty()
+    security_status_color = NumericProperty()
 
     def on_enter(self):
-        pass
+        
+        # Date and Time
+        now = datetime.now()
+        current_time = now.strftime("%I:%M %p")
+        current_date = now.strftime("%B %d, %Y")
+        self.time_date = "As of " + current_time + ", " + current_date
+
+        # Plant Status
+        self.plant_status = "NO DISEASE DETECTED"
+        self.plant_status_color = 1
+        
+        # Security Status
+        self.security_status = "NO INTRUSION DETECTED"
+        self.security_status_color = 1
