4399bfbb553285e753730cc048e6968815052a52
jonsonkurt <jonsonkurt@gmail.com>
2023-01-12 17:51:23 +0800

    Added live feed camera

--- libs/baseclass/homescreen.py
+++ libs/baseclass/homescreen.py
@@ -2,15 +2,21 @@ from kivymd.app import MDApp
 from kivy.lang.builder import Builder
 from kivymd.toast.kivytoast import toast
 from kivy.uix.screenmanager import Screen
-from kivymd.uix.card import MDCard
 from kivymd.uix.dialog import MDDialog
 from datetime import datetime
 from kivy.properties import StringProperty, NumericProperty
 
+import cv2
+from kivy.uix.image import Image
+from kivy.clock import Clock
+from kivy.graphics.texture import Texture
+from kivymd.uix.screen import MDScreen
+from kivy.uix.floatlayout import FloatLayout
+
 Builder.load_file('./libs/kv/homescreen.kv')
 
 class HomeScreen(Screen):
-    
+
     time_date = StringProperty()
     plant_status = StringProperty()
     security_status = StringProperty()
@@ -18,6 +24,24 @@ class HomeScreen(Screen):
     security_status_color = NumericProperty()
 
     def on_enter(self):
+        self.status()
+        self.camera = cv2.VideoCapture(0)
+        self.img = Image()
+        self.layout = FloatLayout()
+        self.layout.add_widget(self.img)
+        self.add_widget(self.layout)
+        Clock.schedule_interval(self.update, 1.0/30.0)
+
+    def update(self, dt):
+        ret, frame = self.camera.read()
+        frame = cv2.rotate(frame, cv2.ROTATE_180)
+
+        if ret:
+            texture = Texture.create(size=(frame.shape[1], frame.shape[0]), colorfmt='bgr')
+            texture.blit_buffer(frame.tostring(), colorfmt='bgr', bufferfmt='ubyte')
+            self.ids.img.texture = texture
+
+    def status(self):
         
         # Date and Time
         now = datetime.now()
@@ -32,3 +56,6 @@ class HomeScreen(Screen):
         # Security Status
         self.security_status = "NO INTRUSION DETECTED"
         self.security_status_color = 1
+
+    def on_stop(self):
+        self.camera.release()
