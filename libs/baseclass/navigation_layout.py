58e2b4d863e4012e4957354d0a60175852397307
jonsonkurt <jonsonkurt@gmail.com>
2023-01-12 23:21:25 +0800

    Added setting time backend base

--- libs/baseclass/navigation_layout.py
+++ libs/baseclass/navigation_layout.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from kivymd.app import MDApp
 from kivy.uix.screenmanager import Screen
 from kivy.lang.builder import Builder
@@ -10,6 +11,16 @@ class NavLayoutScreen(Screen):
  
     def plant_capture_timer(self):
         time_dialog = MDTimePicker()
+        time_dialog.set_time(datetime.now().time())
+        time_dialog.bind(on_save=self.save_time, on_cancel=self.cancel_time)
         time_dialog.open()
-        
-        # print('Set time')
+
+    def save_time(self, instance, time):
+        print('Picked time is', time)
+
+    def cancel_time(self, instance, time):
+        pass
+
+    def set_time(self, instance, time):
+        self.capture_time = time
+        print(self.capture_time)
