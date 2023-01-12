5d306215c49a761b9e06bb793914021085766676
jonsonkurt <jonsonkurt@gmail.com>
2023-01-12 19:17:43 +0800

    Added time picker testing

--- libs/baseclass/navigation_layout.py
+++ libs/baseclass/navigation_layout.py
@@ -2,10 +2,14 @@ from kivymd.app import MDApp
 from kivy.uix.screenmanager import Screen
 from kivy.lang.builder import Builder
 from libs.baseclass import homescreen
+from kivymd.uix.pickers import MDTimePicker
 
 Builder.load_file('./libs/kv/navigation_layout.kv')
 
 class NavLayoutScreen(Screen):
  
     def plant_capture_timer(self):
-        print('timer')
+        time_dialog = MDTimePicker()
+        time_dialog.open()
+        
+        # print('Set time')
