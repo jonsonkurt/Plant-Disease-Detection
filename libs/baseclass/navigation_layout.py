66c71648cb312d638c77f74c5051ef5ca13c3d16
jonsonkurt <jonsonkurt@gmail.com>
2023-01-13 18:43:11 +0800

    Added database for plant scanning time

--- libs/baseclass/navigation_layout.py
+++ libs/baseclass/navigation_layout.py
@@ -1,9 +1,11 @@
 from datetime import datetime
+import sqlite3
 from kivymd.app import MDApp
 from kivy.uix.screenmanager import Screen
 from kivy.lang.builder import Builder
 from libs.baseclass import homescreen
 from kivymd.uix.pickers import MDTimePicker
+from kivymd.toast.kivytoast import toast
 
 # import pyfirmata
 # from pyfirmata import Arduino, util
@@ -41,6 +43,18 @@ class NavLayoutScreen(Screen):
 
     def save_time(self, instance, time):
         print('Picked time is', time)
+        final_time =  str(time)
+        conn = sqlite3.connect("mybase.db")
+        cur = conn.cursor()
+        
+        cur.execute("CREATE TABLE IF NOT EXISTS scanning_time(plant_scanning_time VARCHAR(30))")
+        cur.execute("INSERT INTO scanning_time(plant_scanning_time) VALUES(?)", (final_time,))
+        cur.execute("SELECT * FROM scanning_time")
+        conn.commit()
+
+        toast('Scanning Time Scheduled Successfully.')
+
+        conn.close()
 
     def cancel_time(self, instance, time):
         pass
